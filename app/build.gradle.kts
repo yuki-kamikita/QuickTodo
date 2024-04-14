d38225a0e15277b24fd21c18e070123f11e55c28
yuki_kamikita <akaiyukiusagi@gmail.com>
2024-04-14 22:14:54 +0900

    試しに保存場所を変えてみる

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("release.keystore")
+            storeFile = file("../secrets/release.keystore")
             storePassword = System.getenv("KEYSTORE_PASSWORD")
             keyAlias = System.getenv("KEY_ALIAS")
             keyPassword = System.getenv("KEY_PASSWORD")
