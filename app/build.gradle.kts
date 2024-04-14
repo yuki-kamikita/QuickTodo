733b1ddcb7e30ede2f261d21ca914bede4ab4fd2
yuki_kamikita <akaiyukiusagi@gmail.com>
2024-04-14 16:20:24 +0900

    署名設定追加忘れ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
     }
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
