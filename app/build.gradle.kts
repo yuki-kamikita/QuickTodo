ada3301b68f9076250634baf0349c56bc55188ff
yuki_kamikita <akaiyukiusagi@gmail.com>
2024-04-28 20:44:34 +0900

    🛠️version up 1.2.0(3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.akaiyukiusagi.quicktodo"
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
