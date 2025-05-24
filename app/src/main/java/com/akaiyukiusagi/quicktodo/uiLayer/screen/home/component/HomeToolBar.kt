6b12a7e79661890f6a4b4a8f2b69b39beb234b52
yuki <akaiyukiusagi@gmail.com>
2025-05-24 22:18:50 +0900

    Toolbarをとりあえず入れてみる

---
+++ app/src/main/java/com/akaiyukiusagi/quicktodo/uiLayer/screen/home/component/HomeToolBar.kt
@@ -0,0 +1,97 @@
+package com.akaiyukiusagi.quicktodo.uiLayer.screen.home.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingToolbarDefaults
+import androidx.compose.material3.FloatingToolbarDefaults.floatingToolbarVerticalNestedScroll
+import androidx.compose.material3.HorizontalFloatingToolbar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import com.akaiyukiusagi.quicktodo.uiLayer.PreviewContent
+import com.akaiyukiusagi.quicktodo.uiLayer.ScreenPreviewTemplate
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun HomeToolBar(
+    expanded: Boolean,
+    onAddClick: () -> Unit = {}
+) {
+    HorizontalFloatingToolbar(
+        expanded = expanded,
+        floatingActionButton = {
+            FloatingToolbarDefaults.VibrantFloatingActionButton(
+                onClick = onAddClick
+            ) {
+                Icon(Icons.Filled.Add, "Localized description")
+            }
+        },
+        colors = FloatingToolbarDefaults.vibrantFloatingToolbarColors(),
+        content = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Person, contentDescription = "Localized description")
+            }
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Edit, contentDescription = "Localized description")
+            }
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
+            }
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.MoreVert, contentDescription = "Localized description")
+            }
+        },
+    )
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@ScreenPreviewTemplate
+@Composable
+fun HomeToolBarPreview() {
+    PreviewContent {
+        var expanded by rememberSaveable { mutableStateOf(true) }
+        Scaffold(
+            floatingActionButton = { HomeToolBar(expanded) { expanded = !expanded } },
+            floatingActionButtonPosition = FabPosition.End,
+        ) { innerPadding ->
+            Box(Modifier.padding(innerPadding)) {
+                Column(
+                    Modifier.fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                        .then(
+                            Modifier.floatingToolbarVerticalNestedScroll(
+                                expanded = expanded,
+                                onExpand = { expanded = true },
+                                onCollapse = { expanded = false },
+                            )
+                        )
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Text(text = remember { LoremIpsum().values.first() })
+                }
+            }
+        }
+    }
+}
