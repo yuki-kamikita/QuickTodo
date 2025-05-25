bdbe4aee869c6cea91e159b622177d47f1c6fd13
yuki <akaiyukiusagi@gmail.com>
2025-05-26 00:22:51 +0900

    🚀 ToolbarModeの変更時にいい感じにアニメーションさせる

--- app/src/main/java/com/akaiyukiusagi/quicktodo/uiLayer/screen/home/component/HomeToolBar.kt
+++ app/src/main/java/com/akaiyukiusagi/quicktodo/uiLayer/screen/home/component/HomeToolBar.kt
@@ -1,7 +1,21 @@
 package com.akaiyukiusagi.quicktodo.uiLayer.screen.home.component
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOut
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.animation.with
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -35,17 +49,24 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import com.akaiyukiusagi.quicktodo.R
 import com.akaiyukiusagi.quicktodo.uiLayer.PreviewContent
 import com.akaiyukiusagi.quicktodo.uiLayer.ScreenPreviewTemplate
 import com.akaiyukiusagi.quicktodo.uiLayer.component.ui.layout.Center
 import com.akaiyukiusagi.quicktodo.uiLayer.component.ui.parts.RandomShapeIndicator
+import com.akaiyukiusagi.quicktodo.uiLayer.component.ui.parts.TransparentBackgroundTextField
 import com.akaiyukiusagi.quicktodo.uiLayer.component.ui.parts.initialShapeList
 import com.akaiyukiusagi.quicktodo.uiLayer.screen.home.ToolbarMode
+import kotlin.unaryMinus
 
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalAnimationApi::class)
 @Composable
 fun HomeToolBar(
     expanded: Boolean,
@@ -55,8 +76,11 @@ fun HomeToolBar(
     onSwapClick: () -> Unit = {},
 ) {
     var toolbarMode by remember { mutableStateOf(toolbarMode) }
-    var text by remember { mutableStateOf("") }
     var rememberShape by remember { mutableStateOf(initialShapeList.random()) }
+    var text by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
+    val isFocused = remember { mutableStateOf(false) }
 
     HorizontalFloatingToolbar(
         expanded = expanded,
@@ -64,7 +88,8 @@ fun HomeToolBar(
             FloatingToolbarDefaults.VibrantFloatingActionButton(
                 onClick = {
                     onAddClick()
-                    toolbarMode = ToolbarMode.ADD_TASK
+                    toolbarMode =
+                        if (toolbarMode == ToolbarMode.ACTION) ToolbarMode.ADD_TASK else ToolbarMode.ACTION
                 }
             ) {
                 Icon(Icons.Filled.Add, "Localized description")
@@ -72,31 +97,51 @@ fun HomeToolBar(
         },
 //        colors = FloatingToolbarDefaults.vibrantFloatingToolbarColors(),
         content = {
-            when (toolbarMode) {
-                ToolbarMode.ACTION -> {
-                    IconButton(onClick = { /* TODO */ }) {
-                        Icon(Icons.Default.UnfoldLess, contentDescription = "全部閉じる")
-                    }
-                    IconButton(onClick = { /* TODO */ }) {
-                        Icon(Icons.Default.UnfoldMore, contentDescription = "全部開く")
-                    }
-                    IconButton(
-                        onClick = {
-                            if (!isSwap) rememberShape = initialShapeList.random()
-                            onSwapClick()
-                        },
-                        shape = rememberShape.toShape(),
-                        colors = if (isSwap) IconButtonDefaults.iconButtonColors(containerColor = MaterialTheme.colorScheme.primaryContainer) else IconButtonDefaults.iconButtonColors()
-                    ) {
-                        Icon(Icons.Default.SwapVert, contentDescription = "並び替え")
-                    }
+            AnimatedContent(
+                targetState = toolbarMode,
+                transitionSpec = {
+                    fadeIn(animationSpec = tween()).togetherWith(fadeOut(animationSpec = tween()))
+
+//                    if (targetState == ToolbarMode.ADD_TASK && initialState == ToolbarMode.ACTION) {
+//                        // 右から入って左に出る
+//                        slideInHorizontally { it }.togetherWith(slideOutHorizontally { -it })
+//                    } else {
+//                        // 左から入って右に出る
+//                        slideInHorizontally { -it }.togetherWith(slideOutHorizontally { it })
+//                    }
                 }
-                ToolbarMode.ADD_TASK -> {
-                    TextField(
-                        value = text,
-                        onValueChange = { text = it },
-                        label = { Text("入力してください") }
-                    )
+            ) { mode ->
+                when (mode) {
+                    ToolbarMode.ADD_TASK -> {
+                        TransparentBackgroundTextField(
+                            value = text,
+                            labelText = stringResource(id = R.string.new_task),
+                            focusRequester = focusRequester,
+                            onValueChange = { text = it },
+                            onFocusChanged = { isFocused.value = it },
+                            keyboardDone = { focusManager.clearFocus() }
+                        )
+                    }
+                    ToolbarMode.ACTION -> {
+                        Row {
+                            IconButton(onClick = { /* TODO */ }) {
+                                Icon(Icons.Default.UnfoldLess, contentDescription = "全部閉じる")
+                            }
+                            IconButton(onClick = { /* TODO */ }) {
+                                Icon(Icons.Default.UnfoldMore, contentDescription = "全部開く")
+                            }
+                            IconButton(
+                                onClick = {
+                                    if (!isSwap) rememberShape = initialShapeList.random()
+                                    onSwapClick()
+                                },
+                                shape = rememberShape.toShape(),
+                                colors = if (isSwap) IconButtonDefaults.iconButtonColors(containerColor = MaterialTheme.colorScheme.primaryContainer) else IconButtonDefaults.iconButtonColors()
+                            ) {
+                                Icon(Icons.Default.SwapVert, contentDescription = "並び替え")
+                            }
+                        }
+                    }
                 }
             }
         },
@@ -115,7 +160,7 @@ fun HomeToolBarPreview() {
                 HomeToolBar(
                     expanded = expanded,
                     isSwap = isSwap,
-                    onAddClick = { expanded = !expanded },
+                    onAddClick = { expanded = true },
                     onSwapClick = { isSwap = !isSwap },
                 )
             },
