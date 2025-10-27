e35802c894fdee2007c814e1de2f846a44ca5f84
yuki <akaiyukiusagi@gmail.com>
2025-10-28 01:30:46 +0900

    Column下部のEdge to Edge 対応

--- app/src/main/java/com/akaiyukiusagi/quicktodo/uiLayer/screen/home/HomeScreen.kt
+++ app/src/main/java/com/akaiyukiusagi/quicktodo/uiLayer/screen/home/HomeScreen.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -19,7 +21,6 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.FloatingToolbarDefaults.floatingToolbarVerticalNestedScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -44,6 +45,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -106,10 +108,10 @@ fun HomeScreen(
                 )
             }
         },
-        content = {
+        content = { innerPadding ->
             Surface(
                 modifier = Modifier
-                    .padding(it)
+                    .padding(top = innerPadding.calculateTopPadding())
                     .fillMaxSize()
                     .clickable(
                         interactionSource = remember { MutableInteractionSource() },
@@ -133,6 +135,7 @@ fun HomeScreen(
 //                            ),
                         viewModel = viewModel,
                         settings = settings,
+                        navigationBarHeight = innerPadding.calculateBottomPadding(),
                         snackbarHostState = snackbarHostState
                     )
                 }
@@ -147,6 +150,7 @@ fun TaskList(
     viewModel: IHomeViewModel,
     settings: ISettingsViewModel,
     snackbarHostState: SnackbarHostState,
+    navigationBarHeight: Dp,
     modifier: Modifier = Modifier
 ) {
     val context = LocalContext.current
@@ -229,6 +233,10 @@ fun TaskList(
                     )
                 }
             }
+        }
+        item {
+            // ナビゲーションバーの下に潜り込ませるためにinnerPaddingを使わなかった分、ここで余計にスクロール可能にして帳尻を合わせる
+            Spacer(Modifier.height(navigationBarHeight))
         }
     }
 }
