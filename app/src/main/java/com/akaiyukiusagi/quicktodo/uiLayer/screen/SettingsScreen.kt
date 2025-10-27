2d3a26204c800b99bca2a88f5affc9408fd0582e
yuki <akaiyukiusagi@gmail.com>
2025-10-27 23:54:52 +0900

    🐛 設定画面のレイアウト崩れ修正

--- app/src/main/java/com/akaiyukiusagi/quicktodo/uiLayer/screen/SettingsScreen.kt
+++ app/src/main/java/com/akaiyukiusagi/quicktodo/uiLayer/screen/SettingsScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -106,18 +107,18 @@ fun SettingsScreen(
 //                    SettingsRow("ライセンス", {})
 
                     Spacer(modifier = Modifier.height(12.dp))
-                    Center {
-                        Text(
-                            stringResource(id = R.string.app_name),
-                            style = MaterialTheme.typography.bodyMedium)
-                    }
-                    Center {
-                        Text(
-                            "ver ${BuildConfig.VERSION_NAME}",
-                            style = MaterialTheme.typography.bodySmall,
-                            modifier = Modifier.padding(2.dp))
-                    }
-
+                    Text(
+                        stringResource(id = R.string.app_name),
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        "ver ${BuildConfig.VERSION_NAME}",
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier.fillMaxWidth().padding(2.dp),
+                        textAlign = TextAlign.Center
+                    )
                 }
             }
         }
